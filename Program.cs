f0360a06984442456851aa8421cfc16452797fad
Ankit <159681124+ank920@users.noreply.github.com>
2024-03-03 14:03:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class StringRe
+{
+    static void Main(string[] args)
+    {
+        string originalString = "Hello programmer welcome to the world of programming";
+        string replacedString = originalString.Replace("programmer", "coder");
+
+        Console.WriteLine(replacedString);
+        Console.WriteLine("Ankit Verma");
+        Console.WriteLine("MCA-I A, Roll No: 14");
+
+
+    }
+}
